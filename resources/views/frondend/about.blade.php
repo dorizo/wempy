61dfbf11bd9ea000919538de98c8999019759585
doris hermawan <dorizo89@gmail.com>
2024-05-12 09:58:41 +0700

    sss

--- resources/views/frondend/about.blade.php
+++ resources/views/frondend/about.blade.php
@@ -15,7 +15,6 @@
               
 {{ \Illuminate\Support\Str::limit(strip_tags($profile->menuContent->menucontent), 100, $end='...')  }}
             
-              fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors
             </p>
             <a href="{{URL::to('/halaman/'.$profile->menuCode)}}">
               Read More
